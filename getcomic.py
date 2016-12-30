e60aa394a946581501efd51f7fbe904518c60786
GenuinePotato <a.vandenberg0175@gmail.com>
2016-12-30 01:07:53 -0500

    Added argument passing and directory path
    Can now specify the output location of the images.
    page.html will be removed at the end of the script

--- getcomic.py
+++ getcomic.py
@@ -1,56 +1,66 @@
 # getcomic.py
 # 
-# Modification History
-# 27dec2016,aav Added reg expressions
-# 28dec2016,aav fixed .jpg reg expr to allow spaces for stupid websites that use spaces
-# 29dec2016,aav prepend img names and allow redownloading of sites without duplicates
-# 29dec2016,aav Added suport for gif files
-#
 # Usage
-# curpage - the page that the script will start on
+# curPage - the page that the script will start on
 # endpage - last page to get searched
-# imgline - line in the html that includes the comic image
-# nextimg - line in the html that includes reference to the next html page
+# imgLine - line in the html that includes the comic image
+# nextImg - line in the html that includes reference to the next html page
 
 import os
 import re
+import argparse
 
-curpage = 'http://www.supernormalstep.com/archives/8'
+curPage = 'http://www.supernormalstep.com/archives/8'
 endpage = 'http://www.supernormalstep.com/archives/trouble'
-imgline = 31 - 1
-nextimg = 31 - 1
+imgLine = 31 - 1
+nextImg = 31 - 1
 
-imgnum = 0
-origfiles = os.listdir(os.getcwd())
+#Default output directory
+outDir = os.getcwd()
 
-while curpage != endpage:
-    imgnum = imgnum + 1
-    print('Current page:' + curpage)
+#Arguments and Options parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--directory", help="Specifies a output directory")
+args = parser.parse_args()
+
+#Dealing with the arguments passed in
+if args.directory:
+    outDir = args.directory
+
+print('Output Directory: ' + outDir)
+print()
+
+imgNum = 0
+origFiles = os.listdir(outDir)
+
+while curPage != endpage:
+    imgNum = imgNum + 1
+    print('Current page:' + curPage)
 
     #Get the raw html of the comic page
-    pageget = 'wget "' + curpage + '" -O page.html'
-    os.system(pageget)
+    pageGet = 'wget "' + curPage + '" -O page.html'
+    os.system(pageGet)
 
     #Search the page for comic image
     f = open('page.html', 'r')
     line = f.readlines()
-    searchobj = re.search( r'http[^"]*(jpg|png|gif)', line[imgline])
-    print(searchobj.group())
-    rawname = re.search( r'[^"/]*(jpg|png|gif)', line[imgline])
+    searchObj = re.search( r'http[^"]*(jpg|png|gif)', line[imgLine])
+    print(searchObj.group())
+    rawName = re.search( r'[^"/]*(jpg|png|gif)', line[imgLine])
 
     #Prepend the image with the number to keep them in order
-    savedimgname = str(imgnum).zfill(5) + '-' + rawname.group()
+    savedImgName = str(imgNum).zfill(5) + '-' + rawName.group()
     #Check if the img was already saved
-    if savedimgname not in origfiles:
-        imgget = 'wget "' + searchobj.group() + '" -O "' + savedimgname + '"'
-        os.system(imgget)
+    if savedImgName not in origFiles:
+        imgGet = 'wget "' + searchObj.group() + '" -O "' + outDir + '/' + savedImgName + '"'
+        os.system(imgGet)
 
     #Get the next comic url from the raw html
-    searchobj = re.search( r'<a\b[^>]*next\b[^>]*>', line[nextimg])
-    if searchobj:
-        nextobj = re.search( r'http\b[^"]*', searchobj.group())
-        print(nextobj.group())
-        curpage = nextobj.group()
+    searchObj = re.search( r'<a\b[^>]*next\b[^>]*>', line[nextImg])
+    if searchObj:
+        nextObj = re.search( r'http\b[^"]*', searchObj.group())
+        print(nextObj.group())
+        curPage = nextObj.group()
     else:
         print('reached the end')
 
